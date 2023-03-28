c5eb71cbbff5a4ed76713bba5c0848e5e4eb6649
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2023-03-28 10:09:49 -0300

    updated and tests

--- src/primer/taller/Main.java
+++ src/primer/taller/Main.java
@@ -4,6 +4,7 @@ import edu.princeton.cs.stdlib.StdDraw;
 
 import java.awt.*;
 import java.util.Random;
+import java.util.TreeMap;
 
 public class Main {
 
@@ -12,13 +13,17 @@ public class Main {
     private static final double MIN_VALUE = -1.0;
     private static final double MAX_VALUE = 1.0;
     private static final Color[] COLORS = {Color.RED, Color.BLUE, Color.ORANGE, Color.DARK_GRAY};
-    private static final double LINE_DISTANCE = 00000000.1;
+    private static final double LINE_DISTANCE = 0000000000000000.1;
 
     public static double initialX, initialY, finalX, finalY;
+    public static double velocityVectorX, velocityVectorY = 0.008;
 
     public static void main(String[] args) {
         setScale();
+        StdDraw.enableDoubleBuffering();
         generateLineRandom();
+
+        run();
     }
 
     public static void setScale() {
@@ -43,7 +48,7 @@ public class Main {
                 finalY
         );
 
-        generateParallelLines(4);
+        generateParallelLines(5);
 
     }
 
@@ -66,7 +71,7 @@ public class Main {
 
     }
 
-    public static void selectRandomColor(){
+    public static void selectRandomColor() {
         StdDraw.setPenColor(randomColor());
     }
 
@@ -80,8 +85,38 @@ public class Main {
 
     public static void drawLineAndShow(double initialX, double initialY,
                                        double finalX, double finalY) {
+
+        StdDraw.clear();
+
         StdDraw.line(initialX, initialY, finalX, finalY);
         StdDraw.show();
+    }
+
+    public static void run() {
+        while (true) {
+
+            if (Math.abs(initialX + velocityVectorX * initialX) > 1.0) {
+                velocityVectorX = -velocityVectorX;
+            }
+
+            if (Math.abs(initialY + velocityVectorY * initialY) > 1.0) {
+                velocityVectorY = -velocityVectorY;
+            }
+
+            initialX = initialX + velocityVectorX * initialX;
+            initialY = initialY + velocityVectorY * initialY;
+
+           /* finalX = finalX + velocityVectorX * finalX;
+            finalY = finalY + velocityVectorY * finalY;
+*/
+            drawLineAndShow(initialX, initialY,
+                    finalX, finalY);
+
+            //generateParallelLines(5);
+
+            StdDraw.pause(20);
+
+        }
     }
 
 }
